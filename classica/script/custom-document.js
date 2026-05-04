ef91de3cdf0add9f6803aaf71bfe29cf4e0dd0a0
kamran-barber1 <kamrantbarber@gmail.com>
2026-05-03 23:51:41 -0500

    Add files via upload

---
+++ classica/script/custom-document.js
@@ -0,0 +1,8 @@
+// custom javascript for document view can go here
+
+// Choose which types of filtering you want: sectionnum, or sectiontitle or both
+var _filter_on_types = ["sectiontitle", "sectionnum"];
+// if we have only one type, do we display the option? true/false
+var _display_single_filter_on = true;
+// are titles numeric match? true/false
+var _filter_title_numeric = false;
